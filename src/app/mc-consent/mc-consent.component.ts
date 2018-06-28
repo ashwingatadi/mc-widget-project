cef499a8b956ffb131787698ad20dfa4bde65229
ac899341@gmail.com <vpadmin@CHC.com>
2018-06-28 12:39:07 +0530

    style changes

--- src/app/mc-consent/mc-consent.component.ts
+++ src/app/mc-consent/mc-consent.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ElementRef, Input, ViewEncapsulation, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, Input, ViewEncapsulation,Renderer2, ViewChild } from '@angular/core';
 import { ConfigService } from '../services/config.service';
 import { ApiconnectService } from '../services/apiconnect.service';
 import { NgForm } from '@angular/forms';
@@ -20,22 +20,65 @@ export class McConsentComponent implements OnInit, AfterViewInit {
   @Input() buttonIdForAPICall: string;
   @Input() width: string;
   @Input() height: string;
-  @Input() style: string;
+  @Input() mastercardtheme;
+
   customStyle: Object;
+  @Input() data;
+  public widgetstylenew;
+  private widgetclasses;
+  private widgetstyles;
+  private widgetclassesSplited: String[];
+  private widgetstylesSplited: String[];
   consentList: datatype[] = [];
   @ViewChild('f') form: NgForm;
+  private mastercardtheme1;
 
   constructor(
     private elm: ElementRef, 
     private apiService: ApiconnectService ,
+    private renderer: Renderer2,
     private configService: ConfigService) {
       this.callAPI = elm.nativeElement.getAttribute('callAPI');
       this.buttonIdForAPICall = elm.nativeElement.getAttribute('buttonIdForAPICall');
       this.width = elm.nativeElement.getAttribute('width');
       this.height = elm.nativeElement.getAttribute('height');
-      this.style = elm.nativeElement.getAttribute('style');
 
-      
+
+    this.widgetclasses = elm.nativeElement.getAttribute('widgetclasses');
+    this.widgetstyles = elm.nativeElement.getAttribute('widgetstyles');
+    this.mastercardtheme1 = elm.nativeElement.getAttribute('mastercardtheme');
+    /*if (this.widgetstyle !== null || this.widgetstyle !== undefined || this.widgetstyle !== '') {
+      console.log(this.widgetstyle);
+      console.log(' type of this.widgetstyle');
+      console.log(typeof this.widgetstyle);
+      this.widgetstyleObj = '{' + this.widgetstyle + '}';
+      this.widgetstylenew = JSON.parse(this.widgetstyleObj);
+      console.log('this.widgetstylenew');
+      console.log(this.widgetstylenew);
+      console.log(typeof this.widgetstylenew);
+    } else {
+    }*/
+    var myObj = { "styles":"#WidgetComponent{background:blue;color:white} div#WidgetComponent label{color:pink}"};
+
+    if(this.mastercardtheme1 === 'true'){
+      var mastercarddefaultstyle = myObj.styles;
+      this.appendStyle(mastercarddefaultstyle);
+    }else{
+
+    }
+  }
+
+  appendStyle = function (content) {
+    /*var style = document.createElement('style');
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(content));
+    this.elm.nativeElement.appendChild(style);*/
+
+    const style = this.renderer.createElement('style');
+    const text = this.renderer.createText(content);
+
+    this.renderer.appendChild(style, text);
+    this.renderer.appendChild(this.elm.nativeElement, style);
   }
 
   ngOnInit() {
@@ -61,31 +104,146 @@ export class McConsentComponent implements OnInit, AfterViewInit {
     }
 
     //debugger;
-    if(this.style){
+    /*if(this.style){
       //this.customStyle = JSON.parse(this.style);
       //console.log(this.customStyle);
         this.parseToObject(this.style);
-        // var str = '{' + this.style + '}'; 
+        // var str = '{' + this.style + '}';
         // this.customStyle = Object(this.style);
         // console.log(this.customStyle);
-    }
-    
+    }*/
+
   }
 
+  renderStyle(){
+    var selectedElemDivAll = this.elm.nativeElement.getElementsByTagName('div');
+    var selectedElemDiv = selectedElemDivAll[0];
+
+    if(this.widgetclasses === '' || this.widgetclasses === null || this.widgetclasses === undefined){ }
+    else { // validation check for attributes in component definition
+      this.widgetclassesSplited = this.widgetclasses.split(' ');
+    }
+
+    if(this.widgetstyles === '' || this.widgetstyles === null || this.widgetstyles === undefined) { }
+    else { // validation check for attributes in component definition
+      this.widgetstylesSplited = this.widgetstyles.split(' ');
+    }
+
+
+    //  Class for individual component elements
+    if(this.widgetclassesSplited !== undefined) { // parent block validation
+      for (var i = 0; i < this.widgetclassesSplited.length; i++) {
+
+        var idx = this.widgetclassesSplited[i].indexOf('.');
+        var element = this.widgetclassesSplited[i].substring(0, idx);
+        var elemClass = this.widgetclassesSplited[i].substring(idx + 1, this.widgetclassesSplited[i].length);
+
+        //var currentWidget = document.getElementById('WidgetComponent');  // get component html
+
+        if (element !== 'div') {  // code for non divs
+          //var selectedElemNonDiv = currentWidget.querySelectorAll(element);
+          var selectedElemNonDiv = this.elm.nativeElement.getElementsByTagName(element);
+          if (selectedElemNonDiv.length === 1) { // if only 1 elem
+             this.renderer.addClass(selectedElemNonDiv['0'], elemClass);
+            //selectedElemNonDiv['0'].classList.add(elemClass);
+          }
+          else {
+            for (var j = 0; j < selectedElemNonDiv.length; j++) {
+              // for loop
+              var currIdx = selectedElemNonDiv[j];
+              this.renderer.addClass(currIdx, elemClass);
+              //currIdx.classList.add(elemClass);
+            }
+          }
+        } else { // add class to div parent  element
+          this.renderer.addClass(selectedElemDiv, elemClass);
+          //selectedElemDiv.classList.add(elemClass);
+        }
+      }  // for loop end
+    } // parent block validation check end
+
+    // Inline style for individual or entire component
+
+    if(this.widgetstyles === '' || this.widgetstyles === null || this.widgetstyles === undefined){ } // validation check for attributes in component definition
+    else {
+      if(this.widgetstyles.charAt(0) === '{'){
+        this.widgetstylenew = this.parseToObject(this.widgetstyles);
+      }
+      else {
+        for (var l = 0; l < this.widgetstylesSplited.length; l++) {
+          var idx = this.widgetstylesSplited[l].indexOf('.');
+
+          var hashidxarr =[];
+          //hashidxarr.push(this.widgetstylesSplited[i].indexOf('#'));
+
+          console.log('this.widgetstylesSplited[2]');
+          console.log(this.widgetstylesSplited[l].indexOf('#'));
+
+
+          var element = this.widgetstylesSplited[l].substring(0, idx);
+          var elemStyle = this.widgetstylesSplited[l].substring(idx + 1, this.widgetstylesSplited[l].length);
+          elemStyle = elemStyle.replace(/[{()}]/g, '');
+
+
+         /* if(element.indexOf('#') === -1){
+
+          }else{
+            console.log(element.indexOf('#'));
+            console.log(element);
+            var hashIdx = element.indexOf('#');
+
+            var beforeHash = element.substring(0, hashIdx);
+            var afterHash = element.substring(hashIdx + 1, element.length);
+
+
+            var elemFromDom = document.getElementsByTagName(beforeHash);
+
+          }*/
+
+
+          var currentWidget = document.getElementById('WidgetComponent'); // get component html
+
+          if (element !== 'div') {  // code for non divs
+            var selectedElemNonDiv = this.elm.nativeElement.getElementsByTagName(element); // select non
+            //var selectedElemNonDiv = currentWidget.querySelectorAll(element);
+            if (selectedElemNonDiv.length === 1) { // if only 1 elem
+              this.renderer.setProperty(selectedElemNonDiv['0'], 'style', elemStyle);
+              //selectedElemNonDiv['0'].setAttribute("style", elemStyle);
+            } else {
+              for (var m = 0; m < selectedElemNonDiv.length; m++) {
+                // for loop
+                var currIdx = selectedElemNonDiv[m];
+                this.renderer.setProperty(currIdx, 'style', elemStyle);
+                //currIdx.setAttribute("style",elemStyle);
+              }
+            }
+          } else if(element === 'div') { // add style to parent div
+            this.renderer.setProperty(selectedElemDiv, 'style', elemStyle);
+            //selectedElemDiv.setAttribute("style",elemStyle)
+          }
+        } // end of foor loop
+      } // end of 2nd else  loop
+    } // end of 1st main else  loop
+  }
+
+  ngAfterContentChecked(){
+    this.renderStyle();
+    // alert('ngAfterContentChecked');
+  }
   onSubmitClick(event) {
     console.log(this.form.value);
   }
 
   parseToObject(str: string){
-    //console.log(str);
-    let splStr: string[] = str.split(';')
+    str = str.replace(/[{()}]/g, '');
+
+    let splStr: string[] = str.split(';');
     var keys = [];
     var vals = [];
     var num = 0;
     let frmtedStr: string = '';
 
     splStr.forEach(str => {
-      console.log(str);
       if(str){
         let stlSpl = str.split(":");
         keys.push(stlSpl[0]);
@@ -104,7 +262,7 @@ export class McConsentComponent implements OnInit, AfterViewInit {
     }
     frmtedStr = frmtedStr + "}";
     this.customStyle = JSON.parse(frmtedStr);
-    console.log(JSON.parse(frmtedStr));
+    return this.customStyle;
   }
 }
 
